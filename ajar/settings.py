44bf4986bc96fe17becc191443beef1eeb363fea
Raja1802 <32867085+Raja1802@users.noreply.github.com>
2019-03-16 21:39:33 +0530

    Update settings.py

--- ajar/settings.py
+++ ajar/settings.py
@@ -86,9 +86,9 @@ import pymysql  # noqa: 402
 pymysql.install_as_MySQLdb()
 
 # [START db_setup]
-if os.getenv('GAE_APPLICATION', None):
-    # Running on production App Engine, so connect to Google Cloud SQL using
-    # the unix socket at /cloudsql/<your-cloudsql-connection string>
+# if os.getenv('GAE_APPLICATION', None):
+#     # Running on production App Engine, so connect to Google Cloud SQL using
+#     # the unix socket at /cloudsql/<your-cloudsql-connection string>
     DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.mysql',
@@ -98,22 +98,22 @@ if os.getenv('GAE_APPLICATION', None):
             'NAME': 'ajarani',
         }
     }
-else:
-    # Running locally so connect to either a local MySQL instance or connect to
-    # Cloud SQL via the proxy. To start the proxy via command line:
-    #
-    #     $ cloud_sql_proxy -instances=[INSTANCE_CONNECTION_NAME]=tcp:3306
-    #
-    # See https://cloud.google.com/sql/docs/mysql-connect-proxy
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.mysql',
-            'HOST': '/cloudsql/burnished-block-234705:us-central1:ajarani',
-            'USER': 'ajar',
-            'PASSWORD': '9440261782',
-            'NAME': 'ajarani',
-        }
-    }
+# else:
+#     # Running locally so connect to either a local MySQL instance or connect to
+#     # Cloud SQL via the proxy. To start the proxy via command line:
+#     #
+#     #     $ cloud_sql_proxy -instances=[INSTANCE_CONNECTION_NAME]=tcp:3306
+#     #
+#     # See https://cloud.google.com/sql/docs/mysql-connect-proxy
+#     DATABASES = {
+#         'default': {
+#             'ENGINE': 'django.db.backends.mysql',
+#             'HOST': '/cloudsql/burnished-block-234705:us-central1:ajarani',
+#             'USER': 'ajar',
+#             'PASSWORD': '9440261782',
+#             'NAME': 'ajarani',
+#         }
+#     }
 # [END db_setup]
 #     DATABASES = {
 #         'default': {
