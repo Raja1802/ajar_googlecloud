dc83a3a683db4608da075d25aac2f7d1dacd5449
Raja1802 <32867085+Raja1802@users.noreply.github.com>
2019-03-17 22:18:17 +0530

    Update models.py

--- play/models.py
+++ play/models.py
@@ -27,8 +27,8 @@ class Episode(models.Model):
     def get_absolute_url(self):
         if self.anime.id!=False:
             return reverse('anime_play', kwargs={'id_anime': self.anime.id, 'episode_id': self.id})
-        else:
-            pass
+#         else:
+#             pass
         
     def save(self, force_insert=False, force_update=False):
         super().save(force_insert, force_update)
