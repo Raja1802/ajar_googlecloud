d323b042b6c775a88bb07f3fa1e406e2d42c23d0
Raja1802 <32867085+Raja1802@users.noreply.github.com>
2019-04-10 17:34:08 +0530

    Update sitemaps.py

--- home/sitemaps.py
+++ home/sitemaps.py
@@ -4,10 +4,18 @@ importing_info = apps.get_model('info', 'Anime')
 importing_episode = apps.get_model('play', 'Episode')
 
 
-class TodoSitemap(Sitemap):
+class EpisodeSitemap(Sitemap):
     changefreq = "daily"
     priority = 0.9
     
 
     def items(self):
         return importing_episode.objects.all()[:10]
+    
+class InfoSitemap(Sitemap):
+    changefreq = "daily"
+    priority = 0.9
+    
+
+    def items(self):
+        return importing_info.objects.all()[:10]  
