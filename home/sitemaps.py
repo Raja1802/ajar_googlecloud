b2817928fa8f19d7e476d5c60d225b295db14c35
Raja1802 <32867085+Raja1802@users.noreply.github.com>
2019-04-10 14:27:26 +0530

    Update sitemaps.py

--- home/sitemaps.py
+++ home/sitemaps.py
@@ -9,4 +9,4 @@ class TodoSitemap(Sitemap):
     priority = 0.5
 
     def items(self):
-        return importing_episode.objects.all()[19000:20000]
+        return importing_episode.objects.all()[20000:22000]
